f545792bbdbd5ed71af08c42f8c3f32b13c62f60
kai <b10223052@yuntech.org.tw>
2019-08-21 17:47:11 +0800

    complete banner component

--- src/components/banner/banner-content.js
+++ src/components/banner/banner-content.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { Icon } from 'antd';
+import FacebookSvg from '../../icon/facebook-square-brands.svg';
+import InstagramSvg from '../../icon/instagram-brands.svg';
+import LogoSvg from '../../icon/logo_white.svg';
+import AddressSvg from '../../icon/home-solid.svg';
+import PhoneSvg from '../../icon/phone-alt-solid.svg';
+import EmailSvg from '../../icon/envelope-solid.svg';
 
 function BannerContent() {
 	return (
 		<div className="banner-content">
 			<div className="banner-content__logo">
-				White<br/>Space
+				<Icon component={LogoSvg} />
 			</div>
 			<div className="banner-content__info">
-				<div className="banner-content__info-community"></div>
-				<div className="banner-content__info-detail"></div>
+				<div className="banner-content__info-community">
+					<Icon component={FacebookSvg}/>
+					<Icon component={InstagramSvg}/>
+				</div>
+				<div className="banner-content__info-detail">
+					<div>
+						<Icon component={AddressSvg} />
+						<p>02-17264937</p>
+					</div>
+					<div>
+						<Icon component={PhoneSvg}/>
+						<p>whitespace@whitespace.com.tw</p>
+					</div>
+					<div>
+						<Icon component={EmailSvg}/>
+						<p>台北市羅斯福路十段30號</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './style.scss'
-import bannerImage from '../../images/room_banner.png'
-import BannerContent from './banner-content'
+import React from 'react';
+import './style.scss';
+import bannerImage from '../../images/room_banner.png';
+import BannerContent from './banner-content';
 
-function Banner(){
+function Banner() {
 	return (
 		<div className="banner">
 			<img src={bannerImage}></img>
 			<BannerContent/>
 		</div>
-	)
+	);
 }
 
 export default Banner;
