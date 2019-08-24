ed818ca611b217dbf07009dcbfdb21da8e77813d
kai <b10223052@yuntech.org.tw>
2019-08-24 15:17:57 +0800

    add price content

--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { fetchRoomInfo } from '../../actions/room-info-actions';
 import RoomDetails from '../../components/room-details';
 import RoomInfoBanner from '../../components/room-info-banner';
-
+import './style.scss';
 const propTypes = {
 	room: PropTypes.object,
 	fetchRoomInfo: PropTypes.func,
@@ -18,12 +18,27 @@ const defaultProps = {
 class Room extends Component {
 	render() {
 		const { room } = this.props;
-		const { amenities = {}, imageUrl = [] } =  room;
+		const { amenities = {}, imageUrl = [], holidayPrice, normalDayPrice } =  room;
 
 		return (
-			<div>
+			<div className="room">
 				<RoomInfoBanner imageUrl={imageUrl}/>
-				<RoomDetails amenities={amenities}/>
+				<div className="room-content">
+					<div className="room-content__info">
+						<div className="room-content__info-details">
+							<RoomDetails amenities={amenities}/>
+						</div>
+						<div className="room-content__info-price">
+							<p>NT.{normalDayPrice} </p>
+							<p>平日(一~四)</p>
+							<p>NT.{holidayPrice}</p>
+							<p>假日(五~日)</p>
+						</div>
+					</div>
+					<div className="room-content__calendar"></div>
+
+
+				</div>
 			</div>
 		);
 	}
